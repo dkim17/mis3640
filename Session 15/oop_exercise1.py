c67b00eb7617f03531e4a3e2e1542863dfae539c
Dong Hyun Kim <dkim17@babson.edu>
2018-10-24 20:10:32 -0400

    oop exercise 2

---
+++ Session 15/oop_exercise1.py
@@ -0,0 +1,92 @@
+
+import copy
+
+from oop1_dagim import distance_between_points, Point, Rectangle, print_point
+
+
+class Circle:
+
+
+
+def point_in_circle(point, circle):
+    d = distance_between_points(point, circle.center)
+    print(d)
+    return d <= circle.radius
+
+
+def rect_in_circle(rect, circle):
+    p = copy.copy(rect.corner)
+    print_point(p)
+    if not point_in_circle(p, circle):
+        return False
+
+    p.x += rect.width
+    print_point(p)
+    if not point_in_circle(p, circle):
+        return False
+
+    p.y -= rect.height
+    print_point(p)
+    if not point_in_circle(p, circle):
+        return False
+
+    p.x -= rect.width
+    print_point(p)
+    if not point_in_circle(p, circle):
+        return False
+
+    return True
+
+
+def rect_circle_overlap(rect, circle):
+    p = copy.copy(rect.corner)
+    print_point(p)
+    if point_in_circle(p, circle):
+        return True
+
+    p.x += rect.width
+    print_point(p)
+    if point_in_circle(p, circle):
+        return True
+
+    p.y -= rect.height
+    print_point(p)
+    if point_in_circle(p, circle):
+        return True
+
+    p.x -= rect.width
+    print_point(p)
+    if point_in_circle(p, circle):
+        return True
+
+    return False
+
+
+def main():
+    box = Rectangle()
+    box.width = 100.0
+    box.height = 200.0
+    box.corner = Point()
+    box.corner.x = 50.0
+    box.corner.y = 50.0
+
+    print(box.corner.x)
+    print(box.corner.y)
+
+    circle = Circle
+    circle.center = Point()
+    circle.center.x = 150.0
+    circle.center.y = 100.0
+    circle.radius = 75.0
+
+    print(circle.center.x)
+    print(circle.center.y)
+    print(circle.radius)
+
+    print(point_in_circle(box.corner, circle))
+    print(rect_in_circle(box, circle))
+    print(rect_circle_overlap(box, circle))
+
+
+if __name__ == '__main__':
+    main()
